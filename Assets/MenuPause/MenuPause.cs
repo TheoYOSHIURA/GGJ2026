8502d9323c99c7a1327d3d1f4dfddd4bfc854fc8
bouclette3x3 <bouclette.3x3@gmail.com>
2026-01-31 00:22:35 +0100

    Menu

--- Assets/MenuPause/MenuPause.cs
+++ Assets/MenuPause/MenuPause.cs
@@ -3,8 +3,50 @@ using UnityEngine.SceneManagement;
 
 public class MenuPause : MonoBehaviour
 {
-    public void MainMenu()
+    public GameObject PauseMenu;
+
+    public static bool isPaused;
+
+    void Start()
     {
-        SceneManager.LoadScene("Menu");
+        PauseMenu.SetActive(false);
     }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+    
+        public void MainMenu()
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("Menu");
+        }
+        public void QuitGame()
+        {
+            Application.Quit();
+
+        }
+    public void PauseGame()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+        public void ResumeGame()
+        {
+        PauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            isPaused = false;
+        }
 }
