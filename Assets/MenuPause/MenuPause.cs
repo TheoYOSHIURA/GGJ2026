24a82d88ad27e07540b4d2dd63cebcf29347d05d
bouclette3x3 <bouclette.3x3@gmail.com>
2026-01-30 21:36:25 +0100

    Menu

--- Assets/MenuPause/MenuPause.cs
+++ Assets/MenuPause/MenuPause.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuPause : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void MainMenu()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Menu");
     }
 }
