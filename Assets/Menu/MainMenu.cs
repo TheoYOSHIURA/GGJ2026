800738511d254a1263deb52a52f5a80d5097af84
bouclette3x3 <bouclette.3x3@gmail.com>
2026-02-01 02:35:39 +0100

    Menu

--- Assets/Menu/MainMenu.cs
+++ Assets/Menu/MainMenu.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
+
+
 {
     [SerializeField] GameObject ControlMenu;
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        SceneManager.LoadScene("GameScene");
     }
     public void QuitGame()
     {
---
+++ Assets/Menu/MenuPause.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+using UnityEngine.Playables;
+
+public class MenuPause : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+    public GameObject container;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            playableDirector.Play();
+            container.SetActive(true);
+            Time.timeScale = 0;
+       }
+    }
+    
+        public void MainMenu()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+        }
+        public void QuitGame()
+        {
+            Application.Quit();
+
+        }
+
+        public void ResumeGame()
+        {
+        container.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
